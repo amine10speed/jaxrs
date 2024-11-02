27c396c1ffc882177aef72e45337c2674b4a2ecb
amine10speed <121752633+amine10speed@users.noreply.github.com>
2024-11-02 20:00:19 +0100

    first commit

---
+++ src/main/java/ma/projet/CompteRepository.java
@@ -0,0 +1,8 @@
+package ma.projet;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompteRepository extends JpaRepository<Compte, Long> {
+}
---
+++ src/main/java/ma/projet/TypeCompte.java
@@ -0,0 +1,5 @@
+package ma.projet;
+
+public enum TypeCompte {
+    COURANT, EPARGNE
+}
